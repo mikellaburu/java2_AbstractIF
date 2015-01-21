2bf666fc4c75d890f52c298ba693e1e5fef723d5
mikellaburu <mikellaburu@gmail.com>
2015-01-21 14:20:41 +0100

    Interface y clase Abstract

---
+++ AbstractInterface/src/ClaseAbstract.java
@@ -0,0 +1,8 @@
+
+abstract class ClaseAbstract {
+	public int sumar(int x, int y){
+		return x+y;
+	};
+	
+	abstract int mult(int x,int y);
+}
---
+++ AbstractInterface/src/IF.java
@@ -0,0 +1,5 @@
+
+public interface IF {
+	int sumar(int x, int y);
+	int mult(int x, int y);
+}
